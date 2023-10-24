f699975524934c0a092362204839446011346731
이승헌 <lsh1029@aegisep.com>
2023-10-24 18:06:49 +0900

    add layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div><slot /></div>
+  <div class="flex h-screen w-screen bg-gray-50">
+    <aside class="w-[300px] bg-slate-800">
+      <div class="bg-slate-900 p-2 flex items-center justify-center">
+        <img src="@/assets/images/Logo.svg" alt="이지스원 로고" />
+      </div>
+      <nav class="p-6 text-md text-[#99a2ba]">
+        <ul>
+          <li class="my-2">
+            <Icon name="ic:baseline-dashboard" color="white" size="24" />
+            대시보드
+          </li>
+        </ul>
+      </nav>
+    </aside>
+    <div class="flex-col w-full">
+      <header class="px-2 py-4">header</header>
+      <main><slot /></main>
+    </div>
+  </div>
+
+  <!-- <Sidebar /> -->
+  <!-- <div><slot /></div> -->
 </template>
---
+++ stores/sidebar.ts
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+import sidebarItem from "./data/sidebarItem";
+
+export const useSidebarStore = defineStore("sidebar", () => {
+  const items = ref(sidebarItem);
+  const mobileOpen = ref(false);
+
+  return {
+    items,
+    mobileOpen,
+  };
+});
